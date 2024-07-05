f20004ad4e99daedbd837201b2f2e52f84bfda80
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2024-07-05 17:32:52 +0800

    bug

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="padding: 10px;">
+    <NForm ref="formRef" :model="form" :rules="rules" label-placement="left" label-width="auto" style="width: 200px;">
+      <NFormItem path="email">
+        <NInput v-model="form.email" placeholder="请输入邮箱" clearable />
+      </NFormItem>
+    </NForm>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NInput, NForm, NFormItem } from 'naive-ui'
+
+const formRef = ref()
+
+const form = reactive({
+  email: ''
+})
+const rules = reactive({
+  email: [
+    {
+      required: true,
+      validator: (rule: any, value: any) => {
+        console.log('BUG取不到值', value)
+        return true
+      },
+      trigger: ['input', 'blur']
+    }
+  ]
+})
+</script>
